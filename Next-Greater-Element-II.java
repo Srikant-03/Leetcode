a09806297d749e3fe7c31032c7aa4be0e9a68eba
Srikant-03 <143264460+Srikant-03@users.noreply.github.com>
2025-04-03 14:59:52 +0530

    [503] [Time Beats: 5.92%] [Memory Beats: 82.20%] - LeetPush

--- Next-Greater-Element-II.java
+++ Next-Greater-Element-II.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] answer = new int[n];
-        Arrays.fill(answer, -1); // Default to -1
+        Arrays.fill(answer, -1);
         
         for (int i = 0; i < n; i++) {
             answer[i] = findNextGreater(nums, i);
@@ -13,7 +13,7 @@ class Solution {
 
     private int findNextGreater(int[] nums, int index) {
         int n = nums.length;
-        for (int j = 1; j < n; j++) { // Search circularly
+        for (int j = 1; j < n; j++) { 
             int nextIndex = (index + j) % n;
             if (nums[nextIndex] > nums[index]) {
                 return nums[nextIndex];
