298d719bc39c4026afa5966a4e8521f8838574f4
Srikant-03 <143264460+Srikant-03@users.noreply.github.com>
2026-01-29 13:23:35 +0530

    [344] [Time Beats: 100.00%] [Memory Beats: 91.75%] - LeetPush

---
+++ Reverse-String.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    public void reverseString(char[] s) {
+3        int i = 0;
+4        int j = s.length - 1;
+5        
+6        while(i <= j){
+7            char temp = s[i];
+8            s[i] = s[j];
+9            s[j] = temp;
+10            i++;
+11            j--;
+12        }
+13    }
+14}
