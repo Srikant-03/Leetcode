9dd912a3deee38cab58df7304b9886c4a63fbc5e
Srikant-03 <143264460+Srikant-03@users.noreply.github.com>
2026-01-29 11:01:33 +0530

    [389] [Time Beats: 99.68%] [Memory Beats: 90.45%] - LeetPush

---
+++ Find-the-Difference.java
@@ -0,0 +1,8 @@
+1class Solution {
+2    public char findTheDifference(String s, String t) {
+3        char c = 0;
+4        for(char cs : s.toCharArray()) c ^= cs;
+5        for(char ct : t.toCharArray()) c ^= ct;
+6        return c;
+7    }
+8}
